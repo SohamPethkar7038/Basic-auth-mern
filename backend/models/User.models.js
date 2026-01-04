db4138ae8f0ae8800326cf7c3def5095efce99c8
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-04 23:34:22 +0530

    add user model methods to generate access & refresh tokens

--- backend/models/User.models.js
+++ backend/models/User.models.js
@@ -34,7 +34,31 @@ userSchema.methods.matchPassword=async function(enterPassword){
     return await bcrypt.compare(enterPassword,this.password);
 }
 
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            userName:this.userName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIN: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const User=mongoose.model("User",userScehma);
-
 export default User;
