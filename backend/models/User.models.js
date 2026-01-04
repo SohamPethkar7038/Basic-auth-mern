f82e98352e13d388543c5341bf3044bad46cf88b
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-04 13:19:40 +0530

    add user model and bcrypt password

---
+++ backend/models/User.models.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+
+}, {timestamps:true})
+
+
+userSchema.pre("save",async function(next){
+    
+    if(!this.isModified("password")) next();
+
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt);
+    next();
+})
+
+
+userSchema.methods.matchPasswords=async function(enterPassword){
+    return await bcrypt.compare(enterPassword,this.password);
+}
+
+
+const User=mongoose.model("User",userScehma);
+
+export default User;
