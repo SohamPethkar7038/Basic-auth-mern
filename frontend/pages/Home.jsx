4c603d0cc1043bc4de47396c18a20fab99ea13c4
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-06 11:35:08 +0530

    add home page logic

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
+const Home = ({user,error}) => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'  >
+      <div className='bg-white p-8 rounded-lg shadow-md w-full max-w-lg text-center p-4'>
+        {error && <p className='text-red-500 mb-4 text-sm'>{error}</p>}
+        
+        {user ? (
+          <div>
+            <h2 className='text-2xl font-bold mb-4 text-gray-800'>Welcome, {user.userName}</h2>
+            <p className='text-gray-600'>Email: {user.email}</p>
+          </div>
+        ) :(<div>
+           <h2 className='text-2xl font-bold mb-4 text-gray-800'>Welcome, </h2>
+            <p className='text-gray-700 text-2xl font-bold mb-6'>Please log in or register </p>
+            {/* <div>
+              <Link to='/login'>
+              Login
+              </Link>
+               <Link to='/register'>
+            Register
+              </Link>
+            </div> */}
+        </div>
+
+        )}
+
+      </div>
+    </div>
   )
 }
 
