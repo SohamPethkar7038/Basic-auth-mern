f303a45d4dba7c13b4a3c7f04eeb630654031d2b
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-06 11:59:25 +0530

    add loading... logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import axios from 'axios'
 function App() {
   const [user,setUser]=useState(null)
   const [error,setError]=useState('');
+  const [isLoading,setLoading]=useState(true);
 
   useEffect(()=>{
     const fetchUser=async()=>{
@@ -20,16 +21,26 @@ function App() {
           const res=await axios.get('/api/v1/user/profile',{
             headers:{Authorization: `Bearer ${token}`}
           })
-          setUser(res.data)
+          setUser(res.data.user)
 
         } catch (error) {
           setError("failed to fetch user data") 
           localStorage.removeItem("token")         
         }
       }
+      setLoading(false);
     };
     fetchUser();
   },[])
+
+  if(isLoading){
+    return(
+      <div className='min-h-screen bg-gray-900 flex items-center justify-center'>
+      <div className='text-xl text-white'>Loading...</div>
+
+      </div>
+    )
+  }
 
   return (
     <Router>
@@ -37,8 +48,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home user={user} error={error}/>}/>
-        <Route path="/register" element={<Register setUser={setUser}/>}/>
-        <Route path='/login' element={<Login setUser={setUser}/>}/>
+        <Route path="/register" element={user ? <Navigate to='/' />:<Register setUser={setUser}/>}/>
+        <Route path='/login' element={user ? <Navigate to='/'/> : <Login setUser={setUser}/>}/>
       </Routes>
     </Router>
   )
